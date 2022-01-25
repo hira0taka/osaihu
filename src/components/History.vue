58138550db0a52cd1c18ef1f822b87a93ae821b4
hira0taka <asl125asl74asl415@gmail.com>
2022-01-25 16:08:42 +0900

    mutationとactionの設定

--- src/components/History.vue
+++ src/components/History.vue
@@ -7,58 +7,47 @@
 </template>
 
 <script>
-import axios from "axios";
-import { onMounted, reactive } from "vue";
-import { makeFirebaseURL } from '../const'
-import { mapGetters,mapActions } from 'vuex'
+// import axios from "axios";
+// import { onMounted, reactive } from "vue";
+// import { makeFirebaseURL } from '../const'
+// import { mapGetters,mapActions } from 'vuex'
 
 export default {
   name: "History",
   setup() {
-    const data = reactive({
-      $store.state.payDatas
-      // payData: [
-      //   {
-      //     date: null,
-      //     money: null,
-      //     memo: null,
-      //     shareYou: null,
-      //     sharePrt: null
-      //   }
-      // ]
-    }),
-    // return {
-    //   data
-    // }
-    const getData = () => {
-      data.$store.dispatch('fetchPay')
-    },
-    return{
-      data,
-      setId
-    },
-    const getPayData = async () => {
-    // Pay.jsonのURLを生成
-      const url = makeFirebaseURL('Pay')
-      const payResult = await axios.get(url);
-      // 現状(0115)、firebaseのPay.jsonに入っている3個目（２番）以降のデータがおかしくて、for文でエラーが出るので
-      // slice()関数で1個目と２個目（0番と１番）を切り出しています
-      console.log(payResult.data.slice(0,2))
-      data.payData = payResult.data.slice(0,2);
-    };
+    // const data = reactive({
+    //   $store.state.payDatas
+    // }),
+    const mapActions = () => {
+      store.dispatch('fetchPay')
+    }
+    // const getPayData = async () => {
+    // // Pay.jsonのURLを生成
+    //   const url = makeFirebaseURL('Pay')
+    //   const payResult = await axios.get(url);
+    //   // 現状(0115)、firebaseのPay.jsonに入っている3個目（２番）以降のデータがおかしくて、for文でエラーが出るので
+    //   // slice()関数で1個目と２個目（0番と１番）を切り出しています
+    //   console.log(payResult.data.slice(0,2))
+    //   data.payData = payResult.data.slice(0,2);
+    // };
 
-    onMounted(() => {
-      getPayData();
-    });
-    return { data, getPayData };
+    // onMounted(() => {
+    //   getPayData();
+    // });
+
+    return {
+       mapActions 
+       }
   },
-  methods: {
-  mapActions(['fetchPay']),
-},
-  computed:mapGetters(['allDatas']),
-  crested () {
-    this.feathPay()
+  computed:{
+    mapGetters(){
+      return this.$store.state.payData
+    }
+  },
+  created () {
+    this.fetchPay()
   }
+}
 </script>
 
 <style>
